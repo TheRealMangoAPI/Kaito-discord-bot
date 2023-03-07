9a32c797c9ffef16d73b73fe5b64fb2356fa4690
TheRealMangoAPI <ricardopernack@gmail.com>
2023-03-07 16:45:11 +0100

    Added complete "Cooldown" functionality

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-const DJS = require('discord.js')
-const { IntentsBitField } = require('discord.js')
+const { Client, IntentsBitField, Partials } = require('discord.js')
 const CH = require('command-handler')
 const path = require('path')
 require('dotenv/config')
 
-const client = new DJS.Client({
+const client = new Client({
   intents: [
     IntentsBitField.Flags.Guilds,
     IntentsBitField.Flags.MessageContent,
     IntentsBitField.Flags.GuildMessages,
-    IntentsBitField.Flags.GuildMembers
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.DirectMessages,
+    IntentsBitField.Flags.DirectMessageReactions,
+    IntentsBitField.Flags.DirectMessageTyping,
   ],
+  partials: [Partials.Channel],
 })
 
 client.on('ready', () => {
@@ -20,7 +23,13 @@ client.on('ready', () => {
     client,
     mongoUri: process.env.MONGO_URI,
     commandsDir: path.join(__dirname, 'commands'),
-    testServers: ['1066649397229781052'],
+    testServers: ['1066649397229781052', '1082621430803472494'],
+    botOwners: ['691584005266472960'],
+    cooldownConfig: {
+      errorMessage: 'Please wait {TIME}',
+      botOwnersBypass: false,
+      dbRequired: 300 
+    }
   })
 })
 
