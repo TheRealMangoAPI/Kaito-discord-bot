e341c2896c026dffe3ef705ff5ab9c5ff4dfa71b
Dandeib <dev.dandeib@gmail.com>
2023-06-14 19:34:34 +0200

    help command

--- src/commands/help/help.js
+++ src/commands/help/help.js
@@ -1,4 +1,4 @@
-const { EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require('discord.js');
+const { EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder } = require('discord.js');
 
 module.exports = {
   description: "Displays the help menu",
@@ -19,34 +19,9 @@ module.exports = {
 
     const EmbedMain = new EmbedBuilder()
       .setColor('#40E0D0')
-      .setTitle('overview')
-      .setFields(
-        {
-          name: 'Commands',
-          value: `
-            /list - List's all commands
-            /dashboard - sends you a link to the dashboard
-          `
-        },
-        {
-          name: 'Support & Updates',
-          value: `
-            [<:m_discord:1085222657332621402> Discord](https://mangoapi.dev)
-            [<:LinaHype:933155881552588830> Dashboard](https://mangoapi.dev)
-            [<:LinaLurk:991749890352894032> Website](https://mangoapi.dev)
-          `
-        }
-      )
+      .setTitle('Overview')
+      .setDescription('> \`/help <command>\` ➔ Shows information about a command\n> \`/help <feature>\` ➔ Shows information about a feature\n\n> \`/list commands\` ➔ Lists all commands\n> \`/list features\` ➔ Lists all features\n\n> **Invite Bot:** [Click here](https://example.com)\n> **Discord Server:** [Click here](https://discord.gg/mangoapi)')
 
-      const row = new ActionRowBuilder()
-        .addComponents(
-          new ButtonBuilder()
-          .setCustomId('DashboardButton')
-          .setStyle(ButtonStyle.Link)
-          .setLabel('Dashboard')
-          .setURL('https://mangoapi.dev')
-        )
-      
     return {
       embeds: [EmbedMain]
     }
