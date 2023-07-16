6aba04a3f3a499cca57a384b7f5ccc34420fd58d
TheRealMangoAPI <ricardopernack@gmail.com>
2023-07-16 17:00:47 +0200

    modlogs now with status

--- src/commands/moderation/setup-modlogs.js
+++ src/commands/moderation/setup-modlogs.js
@@ -1,44 +1,76 @@
-const { EmbedBuilder, ApplicationCommandOptionType, Colors, PermissionFlagsBits, ActionRowBuilder, UserSelectMenuBuilder, ChannelSelectMenuBuilder, ChannelType, ButtonBuilder, ButtonStyle, RoleSelectMenuBuilder } = require('discord.js')
-const modLogs = require('../../schemas/mod-logs.schema')
+const {
+  EmbedBuilder,
+  ApplicationCommandOptionType,
+  Colors,
+  PermissionFlagsBits,
+  ActionRowBuilder,
+  UserSelectMenuBuilder,
+  ChannelSelectMenuBuilder,
+  ChannelType,
+  ButtonBuilder,
+  ButtonStyle,
+  RoleSelectMenuBuilder,
+} = require("discord.js");
+const modlogs = require("../../schemas/mod-logs.schema");
 
 module.exports = {
-    description: 'Set Up the logging system',
+  description: "Set Up the logging system",
 
-    type: 'SLASH',
-    testOnly: true,
-    guildOnly: true,
-    ownerOnly: false,
-    permissions: [PermissionFlagsBits.Administrator],
+  type: "SLASH",
+  testOnly: true,
+  guildOnly: true,
+  ownerOnly: false,
+  permissions: [PermissionFlagsBits.Administrator],
 
-    callback: async ({interaction, args}) => {
+  callback: async ({ interaction, args }) => {
+    const DisableButton = new ButtonBuilder()
+      .setCustomId("disableLoggingSystem")
+      .setLabel("Disable")
+      .setStyle(ButtonStyle.Danger);
 
-        
+    const EnableButton = new ButtonBuilder()
+      .setCustomId("enableLoggingSystem")
+      .setLabel("Enable")
+      .setStyle(ButtonStyle.Success);
 
-        return {
-            embeds: [
-                new EmbedBuilder()
-                .setTitle('⚙ Logging system setup')
-                .setDescription(`Choose in which channel you want to send the logs.`)
-                .setColor('#40E0D0')
-            ],
-            components: [
-                new ActionRowBuilder().addComponents(
-                    new ChannelSelectMenuBuilder()
-                    .setChannelTypes(ChannelType.GuildText)
-                    .setCustomId('loggingSetup')
-                    .setPlaceholder('Choose your logging channel')
-                ),
-                new ActionRowBuilder().addComponents(
-                    new ButtonBuilder()
-                    .setCustomId('enableModLogs')
-                    .setLabel('Enable')
-                    .setStyle(ButtonStyle.Success),
-                    new ButtonBuilder()
-                    .setCustomId('disableLoggingSystem')
-                    .setLabel('Disable')
-                    .setStyle(ButtonStyle.Danger),
-                )
-            ]
+    let isEnabled;
+
+    const getEnabled = await modlogs
+      .findOne({ guildId: interaction.guild.id })
+      .then((result) => {
+        if (result) {
+          isEnabled = result.enabled;
+        } else {
+          console.log("No matching document found");
         }
+      })
+      .catch((error) => {
+        console.error("Error retrieving enabledstatus:", error);
+      });
+
+    let button;
+    if (isEnabled) {
+      button = DisableButton;
+    } else {
+      button = EnableButton;
     }
-}
+
+    return {
+      embeds: [
+        new EmbedBuilder()
+          .setTitle("⚙ Logging system setup")
+          .setDescription(`Choose in which channel you want to send the logs.`)
+          .setColor("#40E0D0"),
+      ],
+      components: [
+        new ActionRowBuilder().addComponents(
+          new ChannelSelectMenuBuilder()
+            .setChannelTypes(ChannelType.GuildText)
+            .setCustomId("loggingSetup")
+            .setPlaceholder("Choose your logging channel")
+        ),
+        new ActionRowBuilder().addComponents(button),
+      ],
+    };
+  },
+};
--- src/events/interactionCreate/isButton/open-ticket.js
+++ src/events/interactionCreate/isButton/open-ticket.js
@@ -1,178 +1,203 @@
-const { ChatInputCommandInteraction, ChannelType, PermissionFlagsBits, TextChannel, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, Colors } = require('discord.js')
-const Ticket = require('../../../schemas/ticket-system')
+const {
+  ChatInputCommandInteraction,
+  ChannelType,
+  PermissionFlagsBits,
+  TextChannel,
+  EmbedBuilder,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  Colors,
+} = require("discord.js");
+const Ticket = require("../../../schemas/ticket-system");
 
 /**
- * 
+ *
  * @param {TextChannel} createdChannel
- * @param {ChatInputCommandInteraction} interaction 
+ * @param {ChatInputCommandInteraction} interaction
  */
 module.exports = async (interaction) => {
+  if (interaction.customId === "openTicket") {
+    const ticket = await Ticket.findOne({ guildId: interaction.guild.id });
 
-    if(interaction.customId === 'openTicket') {
+    interaction.guild.channels
+      .create({
+        name: `ticket-${interaction.user.username}`,
+        type: ChannelType.GuildText,
+        permissionOverwrites: [
+          {
+            id: interaction.guild.roles.everyone,
+            deny: [PermissionFlagsBits.ViewChannel],
+          },
+          {
+            id: interaction.user.id,
+            allow: [PermissionFlagsBits.ViewChannel],
+          },
+          ...ticket.team.map((roleId) => ({
+            id: roleId,
+            allow: [PermissionFlagsBits.ViewChannel],
+          })),
+        ],
+      })
+      .then(async (createdChannel) => {
+        const ticket = await Ticket.findOne({ guildId: interaction.guild.id });
 
-        const ticket = await Ticket.findOne({ guildId: interaction.guild.id})
-        
-        interaction.guild.channels.create({
-            name: `ticket-${interaction.user.username}`,
-            type: ChannelType.GuildText,
-            permissionOverwrites: [
-                {
-                    id: interaction.guild.roles.everyone,
-                    deny: [PermissionFlagsBits.ViewChannel]
-                },
-                {
-                    id: interaction.user.id,
-                    allow: [PermissionFlagsBits.ViewChannel]
-                },
-                ...ticket.team.map(roleId => ({
-                    id: roleId,
-                    allow: [PermissionFlagsBits.ViewChannel]
-                }))
-            ]
-        })
-        .then(async (createdChannel) => {
+        let title = `Hello ${interaction.user.username},`;
+        let description =
+          "Our team will take care of you right away.\nPlease describe your problem in the meantime!";
+        let c = "#40E0D0";
 
-            const ticket = await Ticket.findOne({ guildId: interaction.guild.id})
+        if (ticket && ticket.title != "") {
+          title = ticket.title;
+          description = ticket.description;
+          c = ticket.color;
+        }
 
-            let title = `Hello ${interaction.user.username},`
-            let description = 'Our team will take care of you right away.\nPlease describe your problem in the meantime!'
-            let c = '#40E0D0'
+        const currentDate = new Date();
 
-            if(ticket && ticket.title != ""){
-                title = ticket.title
-                description = ticket.description
-                c = ticket.color
-            }
+        const day = String(currentDate.getDate()).padStart(2, "0");
+        const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+        const year = String(currentDate.getFullYear());
 
-            const currentDate = new Date();
+        const formattedDate = `${day}.${month}.${year}`;
 
-            const day = String(currentDate.getDate()).padStart(2, '0');
-            const month = String(currentDate.getMonth() + 1).padStart(2, '0');
-            const year = String(currentDate.getFullYear());
+        title = title
+          .replace("{username}", interaction.user.username)
+          .replace("{userId}", interaction.user.id)
+          .replace("{createdAtDate}", formattedDate);
 
-            const formattedDate = `${day}.${month}.${year}`;
+        description = description
+          .replace("{username}", interaction.user.username)
+          .replace("{userId}", interaction.user.id)
+          .replace("{createdAtDate}", formattedDate);
 
-            title = title.replace('{username}', interaction.user.username).replace('{userId}', interaction.user.id).replace('{createdAtDate}', formattedDate)
+        const hexRegex = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
+        const isValidHex = hexRegex.test(c);
 
-            description = description.replace('{username}', interaction.user.username).replace('{userId}', interaction.user.id).replace('{createdAtDate}', formattedDate)
+        const englishColorRegex =
+          /^(AliceBlue|AntiqueWhite|Aqua|Aquamarine|Azure|Beige|Bisque|Black|BlanchedAlmond|Blue|BlueViolet|Brown|BurlyWood|CadetBlue|Chartreuse|Chocolate|Coral|CornflowerBlue|Cornsilk|Crimson|Cyan|DarkBlue|DarkCyan|DarkGoldenRod|DarkGray|DarkGrey|DarkGreen|DarkKhaki|DarkMagenta|DarkOliveGreen|DarkOrange|DarkOrchid|DarkRed|DarkSalmon|DarkSeaGreen|DarkSlateBlue|DarkSlateGray|DarkSlateGrey|DarkTurquoise|DarkViolet|DeepPink|DeepSkyBlue|DimGray|DimGrey|DodgerBlue|FireBrick|FloralWhite|ForestGreen|Fuchsia|Gainsboro|GhostWhite|Gold|GoldenRod|Gray|Grey|Green|GreenYellow|HoneyDew|HotPink|IndianRed|Indigo|Ivory|Khaki|Lavender|LavenderBlush|LawnGreen|LemonChiffon|LightBlue|LightCoral|LightCyan|LightGoldenRodYellow|LightGray|LightGrey|LightGreen|LightPink|LightSalmon|LightSeaGreen|LightSkyBlue|LightSlateGray|LightSlateGrey|LightSteelBlue|LightYellow|Lime|LimeGreen|Linen|Magenta|Maroon|MediumAquaMarine|MediumBlue|MediumOrchid|MediumPurple|MediumSeaGreen|MediumSlateBlue|MediumSpringGreen|MediumTurquoise|MediumVioletRed|MidnightBlue|MintCream|MistyRose|Moccasin|NavajoWhite|Navy|OldLace|Olive|OliveDrab|Orange|OrangeRed|Orchid|PaleGoldenRod|PaleGreen|PaleTurquoise|PaleVioletRed|PapayaWhip|PeachPuff|Peru|Pink|Plum|PowderBlue|Purple|RebeccaPurple|Red|RosyBrown|RoyalBlue|SaddleBrown|Salmon|SandyBrown|SeaGreen|SeaShell|Sienna|Silver|SkyBlue|SlateBlue|SlateGray|SlateGrey|Snow|SpringGreen|SteelBlue|Tan|Teal|Thistle|Tomato|Turquoise|Violet|Wheat|White|WhiteSmoke|Yellow|YellowGreen)$/;
+        var color;
+        if (isValidHex) {
+          color = c;
+        } else if (englishColorRegex.test(c)) {
+          color = c;
+        } else {
+          color = "Red";
+        }
 
-            const hexRegex = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
-            const isValidHex = hexRegex.test(c);
-
-            const englishColorRegex = /^(AliceBlue|AntiqueWhite|Aqua|Aquamarine|Azure|Beige|Bisque|Black|BlanchedAlmond|Blue|BlueViolet|Brown|BurlyWood|CadetBlue|Chartreuse|Chocolate|Coral|CornflowerBlue|Cornsilk|Crimson|Cyan|DarkBlue|DarkCyan|DarkGoldenRod|DarkGray|DarkGrey|DarkGreen|DarkKhaki|DarkMagenta|DarkOliveGreen|DarkOrange|DarkOrchid|DarkRed|DarkSalmon|DarkSeaGreen|DarkSlateBlue|DarkSlateGray|DarkSlateGrey|DarkTurquoise|DarkViolet|DeepPink|DeepSkyBlue|DimGray|DimGrey|DodgerBlue|FireBrick|FloralWhite|ForestGreen|Fuchsia|Gainsboro|GhostWhite|Gold|GoldenRod|Gray|Grey|Green|GreenYellow|HoneyDew|HotPink|IndianRed|Indigo|Ivory|Khaki|Lavender|LavenderBlush|LawnGreen|LemonChiffon|LightBlue|LightCoral|LightCyan|LightGoldenRodYellow|LightGray|LightGrey|LightGreen|LightPink|LightSalmon|LightSeaGreen|LightSkyBlue|LightSlateGray|LightSlateGrey|LightSteelBlue|LightYellow|Lime|LimeGreen|Linen|Magenta|Maroon|MediumAquaMarine|MediumBlue|MediumOrchid|MediumPurple|MediumSeaGreen|MediumSlateBlue|MediumSpringGreen|MediumTurquoise|MediumVioletRed|MidnightBlue|MintCream|MistyRose|Moccasin|NavajoWhite|Navy|OldLace|Olive|OliveDrab|Orange|OrangeRed|Orchid|PaleGoldenRod|PaleGreen|PaleTurquoise|PaleVioletRed|PapayaWhip|PeachPuff|Peru|Pink|Plum|PowderBlue|Purple|RebeccaPurple|Red|RosyBrown|RoyalBlue|SaddleBrown|Salmon|SandyBrown|SeaGreen|SeaShell|Sienna|Silver|SkyBlue|SlateBlue|SlateGray|SlateGrey|Snow|SpringGreen|SteelBlue|Tan|Teal|Thistle|Tomato|Turquoise|Violet|Wheat|White|WhiteSmoke|Yellow|YellowGreen)$/;
-            var color 
-            if (isValidHex) {
-                color = c
-            } else if (englishColorRegex.test(c)) {
-                color = c
-            } else {
-                color = 'Red'
-            }
-            
-            let teamping = ""
-            ticket.team.forEach((id) => {
-                teamping += `<@&${id}>`
-            })
-            createdChannel.send(teamping)
-            createdChannel.send({ embeds: [
-                new EmbedBuilder()
-                .setTitle(title)
-                .setDescription(description)
-                .setColor(color)
-            ],components: [
-                new ActionRowBuilder().addComponents(
-                    new ButtonBuilder()
-                    .setCustomId('close')
-                    .setLabel('Close')
-                    .setStyle(ButtonStyle.Danger)
-                    .setEmoji('🗑'),
-                    new ButtonBuilder()
-                    .setCustomId('claim')
-                    .setLabel('Claim')
-                    .setStyle(ButtonStyle.Primary)
-                    .setEmoji('🎫')
-                )
-            ]})
-        })
-        .catch(console.error)   
-    }
-
-    if(interaction.customId === 'close') {
-        interaction.channel.delete()
-    }
-
-    if(interaction.customId === 'claim') {
-
-        interaction.channel.permissionOverwrites.edit(interaction.user.id, {
-            ViewChannel: true
-        }); 
-
-        const ticket = await Ticket.findOne({ guildId: interaction.guild.id})
-
-        ticket.team.forEach(roleId => {
-            interaction.channel.permissionOverwrites.edit(roleId, {
-                ViewChannel: false
-            });
+        let teamping = "";
+        ticket.team.forEach((id) => {
+          teamping += `<@&${id}>`;
         });
-
-        interaction.reply({
-            embeds: [
-                new EmbedBuilder()
-                .setDescription('<:verified:1118650085010587688> **You have sucessfull claimed the ticket.**')
-                .setColor(Colors.Green)
-            ],
-            ephemeral: true
-        })
-
-        interaction.message.edit({ components: [
+        createdChannel.send(teamping);
+        createdChannel.send({
+          embeds: [
+            new EmbedBuilder()
+              .setTitle(title)
+              .setDescription(description)
+              .setColor(color),
+          ],
+          components: [
             new ActionRowBuilder().addComponents(
-                new ButtonBuilder()
-                .setCustomId('close')
-                .setLabel('Close')
+              new ButtonBuilder()
+                .setCustomId("close")
+                .setLabel("Close")
                 .setStyle(ButtonStyle.Danger)
-                .setEmoji('🗑'),
-                new ButtonBuilder()
-                .setCustomId('unclaim')
-                .setLabel('Unclaim')
-                .setStyle(ButtonStyle.Danger)
-                .setEmoji('🎫')
-            )
-        ]})
-    }
-    if(interaction.customId === 'unclaim') {
-
-        interaction.channel.permissionOverwrites.delete(interaction.user.id); 
-
-        const ticket = await Ticket.findOne({ guildId: interaction.guild.id})
-
-        ticket.team.forEach(roleId => {
-            interaction.channel.permissionOverwrites.edit(roleId, {
-                ViewChannel: true
-            });
-        });
-
-        interaction.reply({
-            embeds: [
-                new EmbedBuilder()
-                .setDescription('<:verified:1118650085010587688> **You have sucessfull unclaimed the ticket.**')
-                .setColor(Colors.Green)
-            ],
-            ephemeral: true
-        })
-
-        interaction.message.edit({ components: [
-            new ActionRowBuilder().addComponents(
-                new ButtonBuilder()
-                .setCustomId('close')
-                .setLabel('Close')
-                .setStyle(ButtonStyle.Danger)
-                .setEmoji('🗑'),
-                new ButtonBuilder()
-                .setCustomId('claim')
-                .setLabel('Claim')
+                .setEmoji("🗑"),
+              new ButtonBuilder()
+                .setCustomId("claim")
+                .setLabel("Claim")
                 .setStyle(ButtonStyle.Primary)
-                .setEmoji('🎫')
-            )
-        ]})
-    }
-}
+                .setEmoji("🎫")
+            ),
+          ],
+        });
+      })
+      .catch(console.error);
+  }
+
+  if (interaction.customId === "close") {
+    interaction.channel.delete();
+  }
+
+  if (interaction.customId === "claim") {
+    interaction.channel.permissionOverwrites.edit(interaction.user.id, {
+      ViewChannel: true,
+    });
+
+    const ticket = await Ticket.findOne({ guildId: interaction.guild.id });
+
+    ticket.team.forEach((roleId) => {
+      interaction.channel.permissionOverwrites.edit(roleId, {
+        ViewChannel: false,
+      });
+    });
+
+    interaction.reply({
+      embeds: [
+        new EmbedBuilder()
+          .setDescription(
+            "<:verified:1118650085010587688> **You have sucessfull claimed the ticket.**"
+          )
+          .setColor(Colors.Green),
+      ],
+      ephemeral: true,
+    });
+
+    interaction.message.edit({
+      components: [
+        new ActionRowBuilder().addComponents(
+          new ButtonBuilder()
+            .setCustomId("close")
+            .setLabel("Close")
+            .setStyle(ButtonStyle.Danger)
+            .setEmoji("🗑"),
+          new ButtonBuilder()
+            .setCustomId("unclaim")
+            .setLabel("Unclaim")
+            .setStyle(ButtonStyle.Danger)
+            .setEmoji("🎫")
+        ),
+      ],
+    });
+  }
+  if (interaction.customId === "unclaim") {
+    interaction.channel.permissionOverwrites.delete(interaction.user.id);
+
+    const ticket = await Ticket.findOne({ guildId: interaction.guild.id });
+
+    ticket.team.forEach((roleId) => {
+      interaction.channel.permissionOverwrites.edit(roleId, {
+        ViewChannel: true,
+      });
+    });
+
+    interaction.reply({
+      embeds: [
+        new EmbedBuilder()
+          .setDescription(
+            "<:verified:1118650085010587688> **You have sucessfull unclaimed the ticket.**"
+          )
+          .setColor(Colors.Green),
+      ],
+      ephemeral: true,
+    });
+
+    interaction.message.edit({
+      components: [
+        new ActionRowBuilder().addComponents(
+          new ButtonBuilder()
+            .setCustomId("close")
+            .setLabel("Close")
+            .setStyle(ButtonStyle.Danger)
+            .setEmoji("🗑"),
+          new ButtonBuilder()
+            .setCustomId("claim")
+            .setLabel("Claim")
+            .setStyle(ButtonStyle.Primary)
+            .setEmoji("🎫")
+        ),
+      ],
+    });
+  }
+};
